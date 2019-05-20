cf08d68408665df6821202e1dc468bbf5108df77
Alex7498-SV <00068817@uca.edu.sv>
2019-05-20 16:57:19 -0600

    Entidad Autor creada

--- app/src/main/java/com/example/room_library/room/Entities/Autor.kt
+++ app/src/main/java/com/example/room_library/room/Entities/Autor.kt
@@ -1,9 +1,14 @@
 package com.example.room_library.room.Entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "Autor")
 data class Autor(
-
-
-)
+    @ColumnInfo(name = "A_nombre") var nombre : String,
+    @ColumnInfo(name = "A_apellido") var apellido : String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id:Long =0
+}
