cc9ff476f0e925b79f61fe8b3345827a4e11c655
Alex7498-SV <00068817@uca.edu.sv>
2019-05-20 19:01:00 -0600

    me mame(corregido)

--- app/src/main/java/com/example/room_library/room/Entities/Editorial.kt
+++ app/src/main/java/com/example/room_library/room/Entities/Editorial.kt
@@ -1,11 +1,12 @@
 package com.example.room_library.room.Entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Editorial")
 data class Editorial (
-    @PrimaryKey var name : String
+    @ColumnInfo(name = "E_name") var name : String
 ){
     @PrimaryKey(autoGenerate = true)
     var id:Long =0
