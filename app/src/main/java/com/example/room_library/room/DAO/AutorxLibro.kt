67b57aa9e1e903d875c43974c8321c666a48e4b9
Alex7498-SV <00068817@uca.edu.sv>
2019-05-22 10:59:51 -0600

    Libroxeditorial dao creado

--- app/src/main/java/com/example/room_library/room/DAO/AutorxLibro.kt
+++ app/src/main/java/com/example/room_library/room/DAO/AutorxLibro.kt
@@ -3,17 +3,18 @@ package com.example.room_library.room.DAO
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import com.example.room_library.room.Entities.Book
 
 @Dao
 interface AutorxLibro {
 
     @Query("SELECT * FROM Libro INNER JOIN AutorXLibro INNER JOIN Autor " +
             "WHERE Libro.id = AutorXLibro.idLibro AND Autor.id = AutorXLibro.idAutor")
-    fun getAllBooks(): LiveData<List<AutorxLibro>>
+    fun getAllBooks(): LiveData<List<Book>>
 
     @Query("SELECT * FROM Libro INNER JOIN AutorXLibro INNER JOIN Autor " +
             "WHERE Libro.id = AutorXLibro.idLibro AND Autor.A_nombre = (:autor)")
-    fun getBooksbyAutor(autor : String): LiveData<List<AutorxLibro>>
+    fun getBooksbyAutor(autor : String): LiveData<List<Book>>
 
 
 }
--- app/src/main/java/com/example/room_library/room/DAO/LibroXEditorial.kt
+++ app/src/main/java/com/example/room_library/room/DAO/LibroXEditorial.kt
@@ -1,9 +1,18 @@
 package com.example.room_library.room.DAO
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.room_library.room.Entities.Book
 
 @Dao
 interface LibroXEditorial {
+    @Query("SELECT * FROM Libro INNER JOIN LibroXEditorial INNER JOIN Editorial " +
+            "WHERE Libro.id = LibroXEditorial.idLibro AND Editorial.id = LibroXEditorial.idEditorial")
+    fun getAllBooks(): LiveData<List<Book>>
 
+    @Query("SELECT * FROM Libro INNER JOIN LibroXEditorial INNER JOIN Editorial " +
+            "WHERE Libro.id = LibroXEditorial.idLibro AND Editorial.E_name = (:editorial)")
+    fun getBooksbyAutor(editorial : String): LiveData<List<Book>>
 
 }
