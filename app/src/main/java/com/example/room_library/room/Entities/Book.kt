9f2bb8bc034bd508f48aa126fd9b27a5b4d1c719
Alex7498-SV <00068817@uca.edu.sv>
2019-05-21 18:45:32 -0600

    Inicio de una nueva rama para hacer cambios en el repo

--- app/src/main/java/com/example/room_library/room/Entities/Book.kt
+++ app/src/main/java/com/example/room_library/room/Entities/Book.kt
@@ -2,14 +2,23 @@ package com.example.room_library.room.Entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Libro")
+@ForeignKey(
+    entity = Tag::class,
+    parentColumns = ["id"],
+    childColumns = ["tags"],
+    onDelete = CASCADE
+)
+
 class Book(
     @PrimaryKey var ISBN : String,
     @ColumnInfo(name = "L_caratula")var caratula : String,
     @ColumnInfo(name = "L_titulo") var titulo : String,
     @ColumnInfo(name = "L_edicion") var edicion : String,
     @ColumnInfo(name = "L_resumen") var resumen : String,
-    @ColumnInfo(name = "L_tag") var tags : Array<String>
+    @ColumnInfo(name = "L_tag") var tags : String
 )
