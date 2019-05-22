52c1332db7af51203ed049203d1b11ec7e0ce0e8
Daniel hernandez <00209317@uca.edu.sv>
2019-05-21 22:45:04 -0600

    Add changes in BookDao and Book Repository

--- app/src/main/java/com/example/room_library/repository/BookRepository.kt
+++ app/src/main/java/com/example/room_library/repository/BookRepository.kt
@@ -1,9 +1,11 @@
 package com.example.room_library.repository
 
+import androidx.lifecycle.LiveData
 import com.example.room_library.room.DAO.BookDao
+import com.example.room_library.room.Entities.Book
 
 class BookRepository(private val bookDao : BookDao) {
 
-    //val allBooks : LiveData<List<Book>>
+    val allBooks : LiveData<List<Book>> = bookDao.getAllBooks()
 
 }
--- app/src/main/java/com/example/room_library/room/DAO/BookDao.kt
+++ app/src/main/java/com/example/room_library/room/DAO/BookDao.kt
@@ -1,7 +1,9 @@
 package com.example.room_library.room.DAO
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.room_library.room.Entities.Book
 
 @Dao
@@ -10,5 +12,9 @@ interface BookDao {
     @Insert
     //suspend = corrutina
     suspend fun insert(book: Book)
+
+
+    @Query("SELECT * FROM Libro ORDER BY ASC")
+    fun getALLBooks():LiveData<List<Book>>
 
 }
