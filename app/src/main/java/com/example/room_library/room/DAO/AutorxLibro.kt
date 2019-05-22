1da593092311fde85f9f1da27ed4938e39787672
Alex7498-SV <00068817@uca.edu.sv>
2019-05-22 10:49:59 -0600

    Dao autorxlibro agregado

---
+++ app/src/main/java/com/example/room_library/room/DAO/AutorxLibro.kt
@@ -0,0 +1,19 @@
+package com.example.room_library.room.DAO
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface AutorxLibro {
+
+    @Query("SELECT * FROM Libro INNER JOIN AutorXLibro INNER JOIN Autor " +
+            "WHERE Libro.id = AutorXLibro.idLibro AND Autor.id = AutorXLibro.idAutor")
+    fun getAllBooks(): LiveData<List<AutorxLibro>>
+
+    @Query("SELECT * FROM Libro INNER JOIN AutorXLibro INNER JOIN Autor " +
+            "WHERE Libro.id = AutorXLibro.idLibro AND Autor.A_nombre = (:autor)")
+    fun getBooksbyAutor(autor : String): LiveData<List<AutorxLibro>>
+
+
+}
---
+++ app/src/main/java/com/example/room_library/room/DAO/LibroXEditorial.kt
@@ -0,0 +1,9 @@
+package com.example.room_library.room.DAO
+
+import androidx.room.Dao
+
+@Dao
+interface LibroXEditorial {
+
+
+}
